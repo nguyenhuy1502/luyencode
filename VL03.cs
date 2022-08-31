6e3d869683adf376299aaf60ae7ce5c4461a4951
nguyenhuy1502 <90945475+nguyenhuy1502@users.noreply.github.com>
2022-08-31 13:49:33 +0700

    Add files via upload

---
+++ VL03.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            int n = Convert.ToInt32(s1);
+            int s = 0;
+            for(int i=2; i<=n; i++)
+                s = s + i;
+                s = s + 2 * n;
+
+            Console.WriteLine(s);
+        }
+    }
+}
